6d39b95e7c8761c3b0cea2ae0606b8d3bd1fdb3e
Hiep Quach <quachngochiephp@gmail.com>
2025-06-10 08:24:57 +0700

    Xoa tep bo qua

--- clean_vision.py
+++
@@ -1,6 +0,0 @@
-from cleanvision import Imagelab
-
-if __name__ == '__main__':
-    imagelab = Imagelab(data_path = "D:/DeepLearning/Working/Fire_project/Datatest/Train_flame/images")
-    imagelab.find_issues()
-    imagelab.report()
